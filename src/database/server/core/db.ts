a8f416cd4ec5ff607bd9c1fc07452953537c6514
arvinxx <arvinx@foxmail.com>
2024-04-14 22:08:15 +0800

    🚧 wip: add server db schema

---
+++ src/database/server/core/db.ts
@@ -0,0 +1,14 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+import { getServerConfig } from '@/config/server';
+
+const { POSTGRES_DATABASE_URL: connectionString } = getServerConfig();
+
+if (!connectionString) {
+  throw new Error('You are try to use database, but "POSTGRES_DATABASE_URL" is not set correctly');
+}
+
+const client = postgres(connectionString, { prepare: false });
+
+export const serverDB = drizzle(client);
